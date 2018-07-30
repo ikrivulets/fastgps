0fb8851be8566a91a251632900b207c5b0a36bed
Igor Krivulec <ikrivulets@192.168.1.2>
2018-07-30 21:29:57 +0300

    Add more logs

--- api.py
+++ api.py
@@ -2,10 +2,13 @@ import hug
 import logging
 from db.connection import DB
 
+logging.basicConfig(level=logging.DEBUG)
+
 
 @hug.post('/track/gps/{device_id}', versions=1, input_format=hug.input_format.json)
 def post_external_webhook(body, device_id: str):
     try:
+        logging.debug(body)
         db = DB()
         gps = list()
         for el in body['gps']:
@@ -33,7 +36,7 @@ def post_external_webhook(device_id=None):
                                      {'device_id': device_id})
         return {'activity': activity, 'gps': gps}
     except Exception as e:
-        logging.warning(e)
+        logging.error(e)
 
 
 # @hug.get('/track/path}', versions=1, input_format=hug.input_format.json)
@@ -44,6 +47,7 @@ def post_external_webhook(device_id=None):
 @hug.post('/track/activity/{device_id}', versions=1, input_format=hug.input_format.json)
 def post_external_webhook(body, device_id:str):
     try:
+        logging.debug(body)
         db = DB()
         activity = list()
         for el in body['activity']:
@@ -54,4 +58,4 @@ def post_external_webhook(body, device_id:str):
                       values(%(device_id)s, %(activity_type)s, %(ts)s)""",
                       activity)
     except Exception as e:
-        logging.warning(e)
+        logging.error(e)
