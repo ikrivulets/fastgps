0ca573deb24a407940d457937cc5080f7ae6833d
Igor Krivulec <ikrivulets@Igors-MacBook-Pro.local>
2018-08-13 14:17:53 +0300

    Add endpoints for path creation

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 @hug.post('/track/gps/{device_id}', versions=1, input_format=hug.input_format.json)
-def post_external_webhook(body, device_id: str):
+def post_track_gps(body, device_id: str):
     try:
         logging.debug(body)
         db = DB()
@@ -25,7 +25,7 @@ def post_external_webhook(body, device_id: str):
 
 
 @hug.get('/track/gps/{device_id}', versions=1, input_format=hug.input_format.json)
-def post_external_webhook(device_id=None):
+def get_track_gps(device_id=None):
     try:
         if device_id is None:
             return
@@ -39,13 +39,43 @@ def post_external_webhook(device_id=None):
         logging.error(e)
 
 
-# @hug.get('/track/path}', versions=1, input_format=hug.input_format.json)
-# def post_external_webhook(path_id=None, device_id=None):
-#     pass
+@hug.post('/path/', versions=1, input_format=hug.input_format.json)
+def post_track_path(body):
+    if body is None:
+        return {"No parameters are entered"}
+    if body.get('device_id') is None or body.get('start_ts') is None:
+        return {"device_id and start_ts should be populated"}
+    db = DB()
+    device_id = str(body.get('device_id'))
+    start_ts = int(body.get('start_ts'))
+    finish_ts = int(body.get('finish_ts')) if body.get('finish_ts') is not None else None
+    path_id = db.execute_select("""insert into paths (device_id, start_ts, finish_ts) 
+                                values(%(device_id)s, %(start_ts)s, %(finish_ts)s) returning id;""",
+                                {'device_id': device_id, 'start_ts': start_ts, 'finish_ts': finish_ts})
+    return {'path_id': path_id[0]['id']}
+
+
+@hug.put('/path/{path_id}', versions=1, input_format=hug.input_format.json)
+def post_track_path(body, path_id: str):
+    db = DB()
+    if body is None:
+        return {"No parameters are entered"}
+    finish_ts = body.get('finish_ts')
+    if path_id is None or finish_ts is None:
+        return {"path_id and finish_ts should be populated"}
+    path_id = int(path_id)
+    finish_ts = int(finish_ts)
+    path = db.execute_select("select * from paths where id = %(path_id)s",
+                             {'path_id': path_id})
+    if not path:
+        return {'path id {} is absent'.format(path_id)}
+    db.execute_crud("update paths set finish_ts = %(finish_ts)s where id = %(path_id)s",
+                    {'finish_ts': finish_ts, 'path_id': path_id})
+    return {'finish_ts is updated for path id {}'.format(path_id)}
 
 
 @hug.post('/track/activity/{device_id}', versions=1, input_format=hug.input_format.json)
-def post_external_webhook(body, device_id:str):
+def post_track_activiy(body, device_id: str):
     try:
         logging.debug(body)
         db = DB()
--- db/production.sql
+++ db/production.sql
@@ -18,3 +18,12 @@ CREATE TABLE IF NOT EXISTS activity
   activity_type varchar(50),
   ts integer
 );
+
+
+CREATE TABLE IF NOT EXISTS paths
+(
+  id SERIAL,
+  device_id VARCHAR(120),
+  start_ts integer,
+  finish_ts integer
+);
